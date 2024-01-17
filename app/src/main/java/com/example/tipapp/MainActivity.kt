6feeb471e122b45b89bbdbb8cdd6919cd3288590
dantas404 <130323565+dantas404@users.noreply.github.com>
2024-01-17 11:46:11 -0300

    UI Implemented

---
+++ app/src/main/java/com/example/tipapp/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.example.tipapp
+
+import android.icu.text.NumberFormat
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.tipapp.ui.theme.TipAppTheme
+import java.time.temporal.TemporalAmount
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.input.KeyboardType
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TipAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Layout()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Layout(){
+    Column(
+        modifier = Modifier
+            .statusBarsPadding()
+            .padding(horizontal = 40.dp)
+            .safeDrawingPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(R.string.calculate_tip),
+            modifier = Modifier
+                .padding(bottom = 16.dp, top = 40.dp)
+                .align(alignment = Alignment.Start)
+        )
+        NumberField(modifier = Modifier
+            .padding(bottom = 32.dp)
+            .fillMaxWidth())
+        Text(
+            text = stringResource(R.string.tip_amount, "R$0,00"),
+            style = MaterialTheme.typography.displaySmall
+        )
+        Spacer(modifier = Modifier.height(150.dp))
+    }
+}
+
+@Composable
+fun NumberField(modifier:Modifier = Modifier){
+    var amount by remember { mutableStateOf("") }
+    val total:Double = amount.toDoubleOrNull() ?: 0.00
+    val tip = calculate(total)
+    TextField(
+        label = { Text(stringResource(R.string.bill_amount))},
+        value = amount,
+        singleLine = true,
+        onValueChange = {amount = it},
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = modifier
+    )
+}
+
+private fun calculate(amount:Double, percent:Double = 10.0):String{
+    val tip = percent / 100 * amount
+    return NumberFormat.getCurrencyInstance().format(tip)
+}
